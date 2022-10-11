5c7f98dab51628c6fb6625531346d696e7ddc514
EnzoEB12 <enzoexequielbrito11@gmail.com>
2022-10-11 14:09:52 -0300

    Se Implemento el formulario para agregar clientes

--- src/pages/clientes/Clientes.jsx
+++ src/pages/clientes/Clientes.jsx
@@ -5,43 +5,56 @@ import "./Cliente.css";
 import Loading from "../../components/layouts/Loading";
 import { useFetch } from "../../hooks/useFetch";
 import { URL } from "../../utils/getUrl";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSession, useSetSession } from "../../context/SessionProvider";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import {
+  MDBBtn,
+  MDBModal,
+  MDBModalDialog,
+  MDBModalContent,
+  MDBModalHeader,
+  MDBModalTitle,
+  MDBModalBody,
+  MDBModalFooter,
+} from "mdb-react-ui-kit";
 
 function Clientes() {
-    const { setConfigFetch, fetchData, loading, error } = useFetch();
-    const session = useSession();
-    //console.log(session)
-    useEffect(()=>{
-        
-        if(session){
-            setConfigFetch({
-                url: `${URL}/proveedores`,
-                headersRequest: {
-                  method: "GET",
-                  headers: {
-                    "Content-type": "application/json; charset=UTF-8"
-                  }
-                }
-              });
-        }
-    },[])
-
-    //console.log()
-    if(!error){
-        return <h1>Error</h1>
+  const { setConfigFetch, fetchData, loading, error } = useFetch();
+  const session = useSession();
+  //console.log(session)
+  useEffect(() => {
+    if (session) {
+      setConfigFetch({
+        url: `${URL}/proveedores`,
+        headersRequest: {
+          method: "GET",
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        },
+      });
     }
-   
+  }, []);
+
+  //console.log()
+  if (!error) {
+    return <h1>Error</h1>;
+  }
+
+  const [optSmModal, setOptSmModal] = useState(false);
+
+  const toggleShow = () => setOptSmModal(!optSmModal);
+
   return (
     <LayoutContainer>
       <div className="content-wrapper">
         <div className="container-xxl flex-grow-1 container-p-y">
           <div className="container">
             <div className="row">
-              <div class="col-lg-10 mx-auto mb-4">
-                <div class="section-title text-center ">
-                  <h3 class="top-c-sep">LISTA DE CLIENTES/PROVEEDORES</h3>
+              <div className="col-lg-10 mx-auto mb-4">
+                <div className="section-title text-center ">
+                  <h3 className="top-c-sep">LISTA DE CLIENTES/PROVEEDORES</h3>
                   <p>Descripcion de lo que sea Texto.</p>
                 </div>
               </div>
@@ -52,58 +65,99 @@ function Clientes() {
                 <div className="career-search mb-60">
                   <div className="filter-result">
                     <div className="d-md-flex text-capitalize ">
-                    <p className="mb-30 ff-montserrat">
-                      Total De Clientes : {fetchData.length}
-                    </p>
+                      <p className="mb-30 ff-montserrat">
+                        Total De Clientes : {fetchData.length}
+                      </p>
                     </div>
                     <div className="d-md-flex text-capitalize">
-                        <Link to="/Formulario-proveedor" className="menu-link">
-                            <button className="btn btn-infobtn  btn-outline-info " >Agregar Nuevo Cliente</button>
-                        </Link>
+                      <Link to="/Formulario-proveedor" className="menu-link">
+                        <button className="btn btn-infobtn  btn-outline-info">
+                          Agregar Nuevo Cliente
+                        </button>
+                      </Link>
                     </div>
                     <br></br>
                     {!fetchData ? (
-                      <Loading/>
+                      <Loading />
                     ) : (
-                        fetchData.map(item => { 
-                            return(
-                                <div className="job-box d-md-flex align-items-center justify-content-between mb-30">
-                                <div className="job-left my-4 d-md-flex align-items-center flex-wrap">
-                                  <div className="img-holder mr-md-4 mb-md-0 mb-4 mx-auto mx-md-0 d-md-none d-lg-flex">
-                                  {/* Modo Ejemplo */}
-                                  {item?.nombre_proveedor[0]}{item?.nombre_proveedor[10]}
-                                  </div>
-                                  <div className="job-content">
-                                    <h5 className="text-md-left text-center">
-                                    {item?.nombre_proveedor}
-                                    </h5>
-                                    <ul className="d-md-flex text-capitalize ff-open-sans">
-                                      <li className="mr-md-4 mx-3">
-                                        <i className="zmdi zmdi-pin mr-2"></i> {item?.direccion_proveedor}
-                                      </li>
-          
-                                      <li className="mr-md-4">
-                                        <i className="zmdi zmdi-smartphone mr-2"></i> +54
-                                        {item?.telefono_proveedor}
-                                      </li>
-                                    </ul>
-                                  </div>
-                                </div>
-                                <div className="d-md-flex text-capitalize ff-open-sans">
-                                  <button className="btn  d-block w-100  btn-info mx-3">
-                                    Editar
-                                  </button>
-                                  <button className="btn  d-block w-100 btn-danger">
-                                    Eliminar
-                                  </button>
-                                </div>
-                              </div>           
-                            )
-                        }
-                        )
-                    )
-                    }
-                    
+                      fetchData.map((item) => {
+                        return (
+                          <div className="job-box d-md-flex align-items-center justify-content-between mb-30">
+                            <div className="job-left my-4 d-md-flex align-items-center flex-wrap">
+                              <div className="img-holder mr-md-4 mb-md-0 mb-4 mx-auto mx-md-0 d-md-none d-lg-flex">
+                                {/* Modo Ejemplo */}
+                                {item?.nombre_proveedor[0]}
+                                {item?.nombre_proveedor[10]}
+                              </div>
+                              <div className="job-content">
+                                <h5 className="text-md-left text-center">
+                                  {item?.nombre_proveedor}
+                                </h5>
+                                <ul className="d-md-flex text-capitalize ff-open-sans">
+                                  <li className="mr-md-4 mx-3">
+                                    <i className="zmdi zmdi-pin mr-2"></i>{" "}
+                                    {item?.direccion_proveedor}
+                                  </li>
+
+                                  <li className="mr-md-4">
+                                    <i className="zmdi zmdi-smartphone mr-2"></i>{" "}
+                                    +54
+                                    {item?.telefono_proveedor}
+                                  </li>
+                                </ul>
+                              </div>
+                            </div>
+                            <div className="d-md-flex text-capitalize ff-open-sans">
+                              <button className="btn  d-block w-100  btn-info mx-3">
+                                Editar
+                              </button>
+                              <button
+                                className="btn  d-block w-100 btn-danger"
+                                onClick={toggleShow}
+                              >
+                                Eliminar
+                              </button>
+                            </div>
+                            {/* Modal Eliminar */}
+
+                            {/* <MDBBtn onClick={toggleShow}>Small modal</MDBBtn> */}
+                            <MDBModal
+                              show={optSmModal}
+                              tabIndex="-1"
+                              setShow={setOptSmModal}
+                            >
+                              <MDBModalDialog size="sm">
+                                <MDBModalContent>
+                                  <MDBModalHeader className="bg-danger text-white d-flex justify-content-center">
+                                    <MDBModalTitle className="text-white ">
+                                      ¿Quiere Eliminar El Proveedor?
+                                    </MDBModalTitle>
+                                    <MDBBtn
+                                      className="btn-close"
+                                      color="none"
+                                      onClick={toggleShow}
+                                    ></MDBBtn>
+                                  </MDBModalHeader>
+                                  <MDBModalBody className="text-danger d-flex justify-content-center">
+                                  X
+                                  </MDBModalBody>
+                                  <MDBModalFooter>
+                                    <MDBBtn
+                                      className="btn btn-success"
+                                      
+                                      onClick={toggleShow}
+                                    >
+                                      Cancelar
+                                    </MDBBtn>
+                                    <MDBBtn color="danger">Eliminar</MDBBtn>
+                                  </MDBModalFooter>
+                                </MDBModalContent>
+                              </MDBModalDialog>
+                            </MDBModal>
+                          </div>
+                        );
+                      })
+                    )}
                   </div>
                 </div>
 
--- src/pages/clientes/FormCliente.jsx
+++ src/pages/clientes/FormCliente.jsx
@@ -1,15 +1,162 @@
 import Footer from "../../components/layouts/Footer";
 import LayoutContainer from "../../components/layouts/LayoutContainer";
-import "./Cliente.css";
+import "./FormCliente.css";
 
+import { Field, Form, Formik } from "formik";
+import { useEffect, useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import * as yup from "yup";
+import MensajeErrorInput from "../../components/layouts/MensajeErrorInput";
+import { useSetSession } from "../../context/SessionProvider";
+import { useFetch } from "../../hooks/useFetch";
+import { URL } from "../../utils/getUrl";
+import Loading from "../../components/layouts/Loading";
+import Alerta from "../../components/layouts/Alerta";
 
 function FormCliente() {
-    
+  const { setConfigFetch, fetchData, loading, error } = useFetch();
+  const formikRef = useRef();
+
+  const navigate = useNavigate();
+  
+  /* 
+   nombre_proveedor,
+      telefono_proveedor,
+      direccion_proveedor,
+  */
+  const schemaFormCliente = yup.object().shape({
+    nombre_proveedor: yup.string().required("El nombre del proveedor es requerido"),
+    telefono_proveedor: yup.number().required("El telefono del proveedor es requerido"),
+    direccion_proveedor: yup.string().required("La direccion del proveedor es requerida"),
+  });
+
+  const handleSubmit = (values) => {
+    // console.log(values);
+    const {
+      nombre_proveedor,
+      telefono_proveedor,
+      direccion_proveedor
+    } = values;
+
+    setConfigFetch({
+      url: `${URL}/proveedores`,
+      headersRequest: {
+        method: "POST",
+        body: JSON.stringify({
+          nombre_proveedor,
+          telefono_proveedor,
+          direccion_proveedor
+        })
+      }
+    });
+  };
+
+  useEffect(() => {
+    formikRef.current.setSubmitting(false);
+    // console.log(error);
+  }, [error]);
+
+  useEffect(() => {
+    if (fetchData.length === 0) return;
+    navigate("/Proveedores");
+  }, [fetchData]);
+
   return (
     <LayoutContainer>
       <div className="content-wrapper">
         <div className="container-xxl flex-grow-1 container-p-y">
-          <h1>Hola</h1>
+        <div className="container-fluid">
+		<div className="row main-content bg-success text-center">
+			<div className="col-md-4 text-center company__info">
+      <h4 className="company_title">EL LOGO DE SIGAF</h4>
+				{/* <span className="company__logo"><h2><span className="fa fa-android"></span></h2></span>
+				<h4 className="company_title">Your Company Logo</h4> */}
+			</div>
+			<div className="col-md-8 col-xs-12 col-sm-12 login_form ">
+				<div className="container-fluid">
+					<div className="row">
+						<h2 className="h2-formCliente">Formulario Para Clientes/Proveedores</h2>
+					</div>
+					<div className="row">
+          <Formik
+                innerRef={formikRef}
+                initialValues={{
+                  nombre_proveedor: "",
+                  telefono_proveedor: "",
+                  direccion_proveedor:""
+                }}
+                onSubmit={handleSubmit}
+                validationSchema={schemaFormCliente}
+              >
+                {({ isSubmitting }) => (
+						<Form id="formAuthentication" className="form-group">
+							<div className="row">
+                <Field
+                        type="text"
+                        className="form__input"
+                        id="nombre_proveedor"
+                        name="nombre_proveedor"
+                        placeholder="Por favor ingrese el nombre del proveedor"
+                        autoFocus
+                      />
+              </div>
+              <MensajeErrorInput
+                      name="nombre_proveedor"
+                      className="alert alert-danger"
+              />
+							<div className="row">
+								
+              <Field
+                        type="text"
+                        className="form__input"
+                        id="telefono_proveedor"
+                        name="telefono_proveedor"
+                        placeholder="Por favor ingrese el número de telefono del proveedor"
+                        autoFocus
+                      />
+
+							</div>
+              <MensajeErrorInput
+                      name="telefono_proveedor"
+                      className="alert alert-danger"
+                    />
+							<div className="row">
+								
+              <Field
+                        type="text"
+                        className="form__input"
+                        id="direccion_proveedor"
+                        name="direccion_proveedor"
+                        placeholder="Por favor ingrese la direccion del proveedor"
+                        autoFocus
+                      />
+
+							</div>
+              <MensajeErrorInput
+                      name="direccion_proveedor"
+                      className="alert alert-danger"
+                    />
+							<div className="row">
+								<input type="submit"  disabled={isSubmitting} value="Guardar" className="btn_formcliente"/>
+							</div>
+              </Form>
+                )}
+              </Formik>
+					</div>
+          {loading && <Loading />}
+
+        {error?.errors &&
+          error?.errors.map((msgError, i) => (
+            <Alerta
+              claseAlerta="danger"
+              key={i}
+              mensajeAlerta={msgError?.msg}
+            />
+          ))}
+				</div>
+			</div>
+		</div>
+	</div>
         </div>
         <Footer />
         <div className="content-backdrop fade"></div>
